55e93bf77fb9e132b6d4aecbf6b126b2afab47cd
jun68hyk <jun68.hyk@gmail.com>
2024-01-12 06:16:08 +0900

    送信されたemailのユーザーが存在するかどうかの判定

--- app/api/user/login/route.js
+++ app/api/user/login/route.js
@@ -2,11 +2,17 @@
 
 import { NextResponse } from "next/server"
 import connectDB from "@/app/utils/database";
+import { UserModel } from "@/app/utils/schemaModels";
 
-export async function POST() {
+export async function POST(request) {
+    const { email, password } = await request.json()
     try {
         await connectDB()
-        return NextResponse.json({ message: "ログイン成功" })
+        const user = await UserModel.findOne({ email })
+        if (user) {
+            return NextResponse.json({ message: "ログイン成功" })
+        }
+        return NextResponse.json({ message: "ログイン失敗: ユーザー登録をしてください"})
     } catch (err) {
         return NextResponse.json({ message: "ログイン失敗" })
     }
