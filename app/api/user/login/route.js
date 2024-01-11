40871d1ad79817fa712659d4d2e0c3f42d9f6645
jun68hyk <jun68.hyk@gmail.com>
2024-01-12 05:57:50 +0900

    メソッドをPOSTに変更、try〜catch〜追加

--- app/api/user/login/route.js
+++ app/api/user/login/route.js
@@ -1,7 +1,13 @@
 // app/api/user/login/route.js
 
 import { NextResponse } from "next/server"
+import connectDB from "@/app/utils/database";
 
-export async function GET() {
-    return NextResponse.json({message: "ログイン成功"})
+export async function POST() {
+    try {
+        await connectDB()
+        return NextResponse.json({message: "ログイン成功"})
+    } catch (err) {
+        return NextResponse.json({message: "ログイン失敗"})
+    }
 }
