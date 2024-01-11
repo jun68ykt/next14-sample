283f8a03f93fb2863be55d8a9d1935a4b631d7eb
jun68hyk <jun68.hyk@gmail.com>
2024-01-11 17:19:24 +0900

    アイテム１件取得のロジックを実装

---
+++ app/api/item/readsingle/[id]/route.js
@@ -0,0 +1,18 @@
+// app/api/item/readsingle/[id]/route.js
+
+import { NextResponse } from "next/server"
+import connectDB from "@/app/utils/database"
+import { ItemModel } from "@/app/utils/schemaModels";
+
+export async function GET(request, context) {
+    const { id } = context.params
+    console.log(`id: ${id}`)
+
+    try {
+        await connectDB()
+        const item = await ItemModel.findById(id);
+        return NextResponse.json({ message: "アイテム読み取り成功（シングル）", item })
+    } catch(err) {
+        return NextResponse.json({ message: "アイテム読み取り失敗（シングル）" })
+    }
+}
