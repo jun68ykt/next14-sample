6f61a411048827ebf577849db4ca297caf840212
jun68hyk <jun68.hyk@gmail.com>
2024-01-12 07:22:32 +0900

    ミドルウェアを通過するリクエストパスの指定を追加

--- middleware.js
+++ middleware.js
@@ -6,3 +6,11 @@ export async function middleware() {
     console.log("ミドルウェア")
     return NextResponse.next()
 }
+
+export const config = {
+    matcher: [
+        "/api/item/create",
+        "/api/item/update/:path*",
+        "/api/item/delete/:path*",
+    ],
+}
