cff164687ed07f323667d9fa0012549f157b0376
jun68hyk <jun68.hyk@gmail.com>
2024-01-11 14:42:37 +0900

    データベス接続関数(非同期) の作成

---
+++ app/utils/database.js
@@ -0,0 +1,25 @@
+// app/utils/database.js
+
+import mongoose from "mongoose"
+
+const db = {
+    name: "nextAppDataBase",
+    host: "cluster0.jlsyjzk.mongodb.net",
+    user: "jun68ykt",
+    password: "VEAcMixDm6nJUSnq",
+    params: "retryWrites=true&w=majority"
+}
+const dbConnectionUrl = `mongodb+srv://${db.user}:${db.password}@${db.host}/${db.name}?${db.params}`
+
+const connectDB = async () => {
+    console.log("データベースに接続中…")
+    try {
+        await mongoose.connect(dbConnectionUrl)
+        console.log("OK")
+    } catch(err) {
+        console.log("NG")
+        throw err
+    }
+}
+
+export default connectDB
