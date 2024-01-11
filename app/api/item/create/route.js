41002a45681f5799cda4cef190ff6100ce64c367
jun68hyk <jun68.hyk@gmail.com>
2024-01-11 14:45:00 +0900

    アイテム新規作成でデータベース接続を試行

--- app/api/item/create/route.js
+++ app/api/item/create/route.js
@@ -1,9 +1,11 @@
 // app/api/item/create/route.js
 
 import { NextResponse } from "next/server"
+import connectDB from "../../../utils/database"
 
 export async function POST(request) {
     const body = await request.json()
     console.log(body)
+    connectDB()
     return NextResponse.json({message: "アイテム作成"})
 }
