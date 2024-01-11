d7059eadaadcab31265ca033e62a69848a24d701
jun68hyk <jun68.hyk@gmail.com>
2024-01-12 07:06:25 +0900

    ログイン成功時にJWTを生成しレスポンスに含める

--- app/api/user/login/route.js
+++ app/api/user/login/route.js
@@ -1,8 +1,18 @@
 // app/api/user/login/route.js
 
 import { NextResponse } from "next/server"
+import { SignJWT } from "jose";
 import connectDB from "@/app/utils/database";
 import { UserModel } from "@/app/utils/schemaModels";
+
+const getLoginToken = async (email) => {
+    const secretKey = new TextEncoder().encode("next-market-app-book")
+    const payload = { email }
+    return  await new SignJWT(payload)
+        .setProtectedHeader({alg: "HS256"})
+        .setExpirationTime("1d")
+        .sign(secretKey)
+}
 
 export async function POST(request) {
     const { email, password } = await request.json()
@@ -10,8 +20,10 @@ export async function POST(request) {
         await connectDB()
         const user = await UserModel.findOne({ email })
         if (user) {
-            if (password === user.password)
-                return NextResponse.json({ message: "ログイン成功" })
+            if (password === user.password) {
+                const token = await getLoginToken(email)
+                return NextResponse.json({ message: "ログイン成功", token })
+            }
 
             return NextResponse.json({ message: "ログイン失敗: パスワードが間違っています" })
         }
