a13fddb1252c34c255b25327a6171a4435eb3eb1
jun68hyk <jun68.hyk@gmail.com>
2024-01-11 22:46:53 +0900

    アイテム更新のロジックを実装

--- app/api/item/update/[id]/route.js
+++ app/api/item/update/[id]/route.js
@@ -1,7 +1,19 @@
 // app/api/item/update/[id]/route.js
 
 import { NextResponse } from "next/server"
+import connectDB from "@/app/utils/database";
+import { ItemModel } from "@/app/utils/schemaModels";
 
-export async function PUT() {
-    return NextResponse.json({message: "アイテム編集成功"})
+export async function PUT(request, context) {
+    const reqBody = await request.json()
+    const { id } = context.params
+    console.log(reqBody, id)
+    try {
+        await connectDB()
+        await ItemModel.updateOne({ _id: id }, reqBody)
+        return NextResponse.json({message: "アイテム編集成功"})
+    } catch (err) {
+        console.error(err)
+        return NextResponse.json({message: "アイテム編集失敗"})
+    }
 }
