81f88941924beb1cfc4562c89512c3f3d411a974
jun68hyk <jun68.hyk@gmail.com>
2024-01-12 12:57:44 +0900

    シークレットキーをmiddlewareからインポート

--- app/api/user/login/route.js
+++ app/api/user/login/route.js
@@ -4,9 +4,9 @@ import { NextResponse } from "next/server"
 import { SignJWT } from "jose";
 import connectDB from "@/app/utils/database";
 import { UserModel } from "@/app/utils/schemaModels";
+import { secretKey } from "@/middleware";
 
 const getLoginToken = async (email) => {
-    const secretKey = new TextEncoder().encode("next-market-app-book")
     const payload = { email }
     return  await new SignJWT(payload)
         .setProtectedHeader({alg: "HS256"})
