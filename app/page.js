40f4973f6fdad18b436ec1de6009e31757f454bf
jun68hyk <jun68.hyk@gmail.com>
2024-01-14 04:31:57 +0900

    テスト表示

---
+++ app/page.js
@@ -0,0 +1,12 @@
+// app/page.js
+
+const ReadAllItems = () => {
+  return (
+      <div>
+        <h1 className="h1-style">こんにちは</h1>
+        <h3>さようなら</h3>
+      </div>
+  )
+}
+
+export default ReadAllItems
