107a0599376c9010a6597fdebb1429c180e67981
jun68hyk <jun68.hyk@gmail.com>
2024-01-14 13:00:40 +0900

    ユーザー登録画面の登録フォームを実装

--- app/user/register/page.js
+++ app/user/register/page.js
@@ -1,18 +1,46 @@
 "use client"
+import { useState } from "react"
 
 const UserRegister = () => {
 
-  const handleSubmit = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    try {
+      const options = {
+        method: 'POST',
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, email, password })
+      }
+      const res = await fetch("http://localhost:3000/api/user/register", options)
+      const resBody = await res.json()
+      if (!res.ok)
+        throw new Error(resBody.detail)
+
+      alert(`ユーザー登録成功`)
+    } catch(err) {
+      alert(`ユーザー登録失敗\n\n理由: ${err.message}`)
+    }
+  }
+
+  const handleChange = ({ target }) => {
+    const setter = { name: setName, email: setEmail, password: setPassword}[target.name]
+    setter(target.value)
   }
 
   return (
     <div>
       <h1>ユーザー登録</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="name" placeholder="名前" required/>
-        <input type="text" name="email" placeholder="メールアドレス" required/>
-        <input type="text" name="password" placeholder="パスワード" required/>
+      <form onSubmit={handleSubmit} method="post">
+        <input type="text" name="name" onChange={handleChange} placeholder="名前" required/>
+        <input type="text" name="email" onChange={handleChange} placeholder="メールアドレス" required/>
+        <input type="text" name="password" onChange={handleChange} placeholder="パスワード" required/>
         <button>登録</button>
       </form>
     </div>
