a8d6742141f63d2582785e4c4ee9a08dc7949ef3
jun68hyk <jun68.hyk@gmail.com>
2024-01-14 11:01:14 +0900

    ユーザー登録フォームを追加
    このままだと下記のエラー
     ⨯ Error: Event handlers cannot be passed to Client Component props.
      <form onSubmit={function} children=...>
                     ^^^^^^^^^^
    If you need interactivity, consider converting part of this to a Client Component.

--- app/user/register/page.js
+++ app/user/register/page.js
@@ -1,7 +1,18 @@
 const UserRegister = () => {
+
+  const handleSubmit = () => {
+
+  }
+
   return (
     <div>
       <h1>ユーザー登録</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="名前" required/>
+        <input type="text" name="email" placeholder="メールアドレス" required/>
+        <input type="text" name="password" placeholder="パスワード" required/>
+        <button>登録</button>
+      </form>
     </div>
   )
 }
