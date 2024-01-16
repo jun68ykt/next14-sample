280b4f5b98d4cdf6c0a4108f2e4f7773d8521258
jun68hyk <jun68.hyk@gmail.com>
2024-01-16 20:44:38 +0900

    フォームのサブミットハンドラ作成

--- app/user/login/page.js
+++ app/user/login/page.js
@@ -11,10 +11,19 @@ const UserLogin = () => {
     setter?.(value)
   }
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    try {
+      // TODO: ログインAPIをリクエスト
+    } catch (err) {
+    }
+  }
+
   return (
     <div>
       <h1>ログイン</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" name="email" onChange={handleChange} placeholder="メールアドレス" required/>
         <input type="text" name="password" onChange={handleChange} placeholder="パスワード" required/>
         <button>ログイン</button>
