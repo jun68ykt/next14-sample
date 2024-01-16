9ea2e2677693185544c21b3b4335303dc6dc708b
jun68hyk <jun68.hyk@gmail.com>
2024-01-16 23:44:49 +0900

    アイテム詳細ページを実装

--- app/item/readsingle/[id]/page.js
+++ app/item/readsingle/[id]/page.js
@@ -1,9 +1,28 @@
 // app/item/readsingle/[id]/page.js
+import Image from "next/image";
 
-const ReadSingleItem = (context) => {
-  console.log(context)
+const getSingleItem = async (id) => {
+  const url = `http://localhost:3000/api/item/readsingle/${id}`
+  const resp = await fetch(url)
+  const { item } = await resp.json()
+  return item
+}
+
+const ReadSingleItem = async (context) => {
+  const { image, title, price, description } = await getSingleItem(context.params.id)
+
   return (
-    <h1>個別アイテムページ</h1>
+    <div>
+      <div>
+        <Image src={image} width={750} height={500} alt="item-image" priority />
+      </div>
+      <div>
+        <h1>{title}</h1>
+        <h2>{price}</h2>
+        <hr/>
+        <p>{description}</p>
+      </div>
+    </div>
   )
 }
 
