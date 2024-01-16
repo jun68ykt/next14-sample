945b82c1a4123c77e8977d75e301d59c29720a85
jun68hyk <jun68.hyk@gmail.com>
2024-01-16 20:34:33 +0900

    2つのinput共通のchangeハンドラを作成

--- app/user/login/page.js
+++ app/user/login/page.js
@@ -1,12 +1,22 @@
 "use client"
 
+import { useState } from "react";
+
 const UserLogin = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleChange = ({ target: { name, value }}) => {
+    const setter = { email: setEmail, password: setPassword }[name]
+    setter?.(value)
+  }
+
   return (
     <div>
       <h1>ログイン</h1>
       <form>
-        <input type="text" name="email" placeholder="メールアドレス" required/>
-        <input type="text" name="password" placeholder="パスワード" required/>
+        <input type="text" name="email" onChange={handleChange} placeholder="メールアドレス" required/>
+        <input type="text" name="password" onChange={handleChange} placeholder="パスワード" required/>
         <button>ログイン</button>
       </form>
     </div>
