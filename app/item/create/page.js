92608ebe86f84014058be48d696bfcefdeffffaa
jun68hyk <jun68.hyk@gmail.com>
2024-01-17 00:11:43 +0900

    登録するアイテム内容を保持するオブジェクトをstateにし、changeハンドラを追加

--- app/item/create/page.js
+++ app/item/create/page.js
@@ -1,14 +1,25 @@
 "use client"
 
+import {useState} from "react";
+
+const INIT_ITEM = {title: "", price: "", image: "", description: ""}
+
 const ItemCreate = () => {
+  const [item, setItem] = useState(INIT_ITEM)
+  console.log("item", item)
+
+  const handleChange = ({target: {name, value}}) => {
+    setItem({...item, [name]: value})
+  }
+
   return (
     <div>
       <h1>アイテム作成</h1>
       <form>
-        <input type="text" name="title" placeholder="アイテム名" required/>
-        <input type="text" name="price" placeholder="価格" required/>
-        <input type="text" name="image" placeholder="画像" required/>
-        <textarea name="description" rows={15} placeholder="商品説明" required></textarea>
+        <input type="text" name="title" placeholder="アイテム名" onChange={handleChange} required/>
+        <input type="text" name="price" placeholder="価格" onChange={handleChange} required/>
+        <input type="text" name="image" placeholder="画像" onChange={handleChange} required/>
+        <textarea name="description" rows={15} placeholder="商品説明" onChange={handleChange} required></textarea>
         <button>作成</button>
       </form>
     </div>
