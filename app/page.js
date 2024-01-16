d9f693b600e894a2bfe875331e26230924576729
jun68hyk <jun68.hyk@gmail.com>
2024-01-16 22:40:33 +0900

    トップで表示するアイテム一覧コンポーネント(サーバーコンポーネントであることに注意)

--- app/page.js
+++ app/page.js
@@ -1,11 +1,28 @@
 // app/page.js
 
-const ReadAllItems = () => {
+const getAllItems = async () => {        // 追加
+  const resp = await fetch("http://localhost:3000/api/item/readall")
+  const {items} = await resp.json()
+  return items
+}
+
+const ItemBox = ({item}) => (
+  <div>
+    <img src={item.image} alt="" />
+    <h2>{item.price}</h2>
+    <h3>{item.title}</h3>
+    <p>{item.description}</p>
+  </div>
+)
+
+const ReadAllItems = async () => {
+  const items = await getAllItems()
+
   return (
-      <div>
-        <h1 className="h1-style">こんにちは</h1>
-        <h3>さようなら</h3>
-      </div>
+    <div>
+      <h1 className="h1-style">こんにちは</h1>
+      {items.map(item => <ItemBox item={item} />)}
+    </div>
   )
 }
 
