25fc193c2b2f01e54e97b84af03655562c40a445
jun68hyk <jun68.hyk@gmail.com>
2024-01-16 20:02:50 +0900

    メールアドレスとパスワードを入力するフォームを追加

--- app/user/login/page.js
+++ app/user/login/page.js
@@ -4,6 +4,11 @@ const UserLogin = () => {
   return (
     <div>
       <h1>ログイン</h1>
+      <form>
+        <input type="text" name="email" placeholder="メールアドレス" required/>
+        <input type="text" name="password" placeholder="パスワード" required/>
+        <button>ログイン</button>
+      </form>
     </div>
   )
 }
