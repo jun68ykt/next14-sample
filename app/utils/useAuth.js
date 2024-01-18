89605f2fc5f846095315c02145b78b6c71e5a063
jun68hyk <jun68.hyk@gmail.com>
2024-01-19 03:38:10 +0900

    ログインユーザーのemailを取得するカスタムフック作成

---
+++ app/utils/useAuth.js
@@ -0,0 +1,31 @@
+// utils/useAuth.js
+import {useEffect, useState} from "react"
+import {useRouter} from "next/navigation"
+import {jwtVerify} from "jose"
+import {secretKey} from "@/middleware"
+
+const useAuth = () => {
+  const [loginUserEmail, setLoginUserEmail] = useState("")
+  const router = useRouter()
+
+  useEffect(() => {
+    (async () => {
+      const token = localStorage.getItem("token")
+      if (!token) {
+        router.push("/user/login")
+        return
+      }
+      try {
+        const decodedJWT = jwtVerify(token, secretKey)
+        setLoginUserEmail(decodedJWT.payload.email)
+      } catch (error) {
+        localStorage.removeItem("token")
+        router.push("/user/login")
+      }
+    })()
+  }, [router]);
+
+  return {loginUserEmail}
+}
+
+export default useAuth
