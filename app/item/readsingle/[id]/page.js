a3608fe068b99366e912e620fc6300c58fe236ce
jun68hyk <jun68.hyk@gmail.com>
2024-01-19 03:04:15 +0900

    アイテム詳細に編集ページと削除ページへのリンクを追加

--- app/item/readsingle/[id]/page.js
+++ app/item/readsingle/[id]/page.js
@@ -1,5 +1,6 @@
 // app/item/readsingle/[id]/page.js
 import Image from "next/image";
+import Link from "next/link";
 
 const getSingleItem = async (id) => {
   const url = `http://localhost:3000/api/item/readsingle/${id}`
@@ -9,7 +10,7 @@ const getSingleItem = async (id) => {
 }
 
 const ReadSingleItem = async (context) => {
-  const { image, title, price, description } = await getSingleItem(context.params.id)
+  const { _id, image, title, price, description } = await getSingleItem(context.params.id)
 
   return (
     <div>
@@ -21,6 +22,10 @@ const ReadSingleItem = async (context) => {
         <h2>{price}</h2>
         <hr/>
         <p>{description}</p>
+        <div>
+          <Link href={`/item/update/${_id}`}>アイテム編集</Link>
+          <Link href={`/item/delete/${_id}`}>アイテム削除</Link>
+        </div>
       </div>
     </div>
   )
