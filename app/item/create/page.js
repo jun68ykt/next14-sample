a472492f279fd31d891ac41937eb0a512e9e95e2
jun68hyk <jun68.hyk@gmail.com>
2024-01-19 11:19:10 +0900

    loginUserEmailがtruethyの場合のみフォームを表示

--- app/item/create/page.js
+++ app/item/create/page.js
@@ -38,16 +38,18 @@ const ItemCreate = () => {
   }
 
   return (
-    <div>
-      <h1>アイテム作成</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="title" placeholder="アイテム名" onChange={handleChange} required/>
-        <input type="text" name="price" placeholder="価格" onChange={handleChange} required/>
-        <input type="text" name="image" placeholder="画像" onChange={handleChange} required/>
-        <textarea name="description" rows={15} placeholder="商品説明" onChange={handleChange} required></textarea>
-        <button>作成</button>
-      </form>
-    </div>
+    loginUserEmail && (
+      <div>
+        <h1>アイテム作成</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="title" placeholder="アイテム名" onChange={handleChange} required/>
+          <input type="text" name="price" placeholder="価格" onChange={handleChange} required/>
+          <input type="text" name="image" placeholder="画像" onChange={handleChange} required/>
+          <textarea name="description" rows={15} placeholder="商品説明" onChange={handleChange} required></textarea>
+          <button>作成</button>
+        </form>
+      </div>
+    )
   )
 }
 
